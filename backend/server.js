c65c8b5c03e0fc343beec14e7a67abd9816923cb
June Zhao <bz.bijun@gmail.com>
2022-12-05 13:07:23 -0800

    final

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
@@ -21,8 +23,6 @@ app.use("/api/tickets", require("./routes/ticketRoutes"));
 // Serve Frontend
 if (process.env.NODE_ENV === "production") {
   // Set build folder as static
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
   // FIX: below code fixes app crashing on refresh in deployment
   //   app.get("*", (_, res) => {
   //     res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
