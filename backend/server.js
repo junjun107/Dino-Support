33470769f0ec2562f8ee1d2ecc761bfae691023c
June Zhao <bz.bijun@gmail.com>
2022-12-15 13:37:51 -0800

    clean up

--- backend/server.js
+++ backend/server.js
@@ -6,41 +6,22 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
-// Connect to database
-// connectDB();
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Serve Frontend
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 
-// Serve Frontend
-if (process.env.NODE_ENV === "production") {
-  // Set build folder as static
-  // FIX: below code fixes app crashing on refresh in deployment
-  //   app.get("*", (_, res) => {
-  //     res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-  //   });
-  // } else {
-  //   app.get("/", (req, res) => {
-  //     res.status(200).json({ message: "Welcome to the Support Desk API" });
-  //   });
-}
-
 app.use(errorHandler);
 
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-//new comment
-//Connect To Database
+//Connect To Database before listening port
 connectDB().then(() => {
-  //Server Running
-  console.log("I am here!!!!!");
   app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}, you better catch it!`);
   });
